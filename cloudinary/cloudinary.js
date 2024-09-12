7d05fbb14f5e9c42d658a25be948fa5977da2c7f
Junaid <junaiddshaukat@gmail.com>
2024-09-12 14:06:41 +0500

    cloudinary file upload

---
+++ cloudinary/cloudinary.js
@@ -0,0 +1,43 @@
+// General code to upload file on cloudinary with production level practices
+
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded successfullt
+    console.log("file is uploaded on clouinart", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the localy saved temporary file as the upload operation got failed
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
+
+//     METHOD 2          //
+
+// const uploadResult = await cloudinary.uploader
+//   .upload(
+//     "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
+//     {
+//       public_id: "shoes",
+//     }
+//   )
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// console.log(uploadResult);
